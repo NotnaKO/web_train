43546f3613e5b2082851f7a037483497203e5b23
NotnaKO <kopanovanton20062004>
2020-03-17 20:47:16 +0300

    Ещё немного правок восле создания

--- data/articles_resoursce.py
+++ data/articles_resoursce.py
@@ -1,28 +1,29 @@
 from flask import *
-from .db_session import create_session
-from .articles import Article
 from flask_restful import reqparse, abort, Resource
+
+from .articles import Articles
+from .db_session import create_session
 
 
 def abort_if_article_not_found(article_id):
     session = create_session()
-    article = session.query(Article).get(article_id)
+    article = session.query(Articles).get(article_id)
     if not article:
         abort(404, message=f"article {article_id} not found")
 
 
-class ArticleResource(Resource):
+class ArticlesResource(Resource):
     def get(self, article_id):
         abort_if_article_not_found(article_id)
         session = create_session()
-        article = session.query(Article).get(article_id)
+        article = session.query(Articles).get(article_id)
         return jsonify({'article': article.to_dict(
             only=('id', 'author', 'header', 'text_address'))})
 
     def delete(self, article_id):
         abort_if_article_not_found(article_id)
         session = create_session()
-        article = session.query(Article).get(article_id)
+        article = session.query(Articles).get(article_id)
         session.delete(article)
         session.commit()
         return jsonify({'success': 'OK'})
@@ -34,17 +35,17 @@ parser.add_argument('header', required=True)
 parser.add_argument('text_address', required=True, type=int)
 
 
-class ArticleListResource(Resource):
+class ArticlesListResource(Resource):
     def get(self):
         session = create_session()
-        article = session.query(Article).all()
+        article = session.query(Articles).all()
         return jsonify({'article': [item.to_dict(
             only=('author', 'header', 'text_address')) for item in article]})
 
     def post(self):
         args = parser.parse_args()
         session = create_session()
-        article = Article(author=args['author'], header=args['header'], text_address=args['text_address'])
+        article = Articles(author=args['author'], header=args['header'], text_address=args['text_address'])
         session.add(article)
         session.commit()
         return jsonify({'success': 'OK'})

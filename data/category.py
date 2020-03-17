7770d33916302804cc8dc78669459e98747fa789
NotnaKO <kopanovanton20062004>
2020-03-17 20:15:43 +0300

    Немного изменений с классами базы данных и исправлений ошибок

--- data/category.py
+++ data/category.py
@@ -1,10 +1,11 @@
 import sqlalchemy
+
 from .db_session import SqlAlchemyBase
 from .users import SerializerMixin
 
 association_table = sqlalchemy.Table('association', SqlAlchemyBase.metadata,
                                      sqlalchemy.Column('articles', sqlalchemy.Integer,
-                                                       sqlalchemy.ForeignKey('articles.id')),
+                                                       sqlalchemy.ForeignKey('article.id')),
                                      sqlalchemy.Column('category', sqlalchemy.Integer,
                                                        sqlalchemy.ForeignKey('category.id'))
                                      )
